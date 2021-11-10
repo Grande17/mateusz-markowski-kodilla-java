e6d3e8734a9a122925b0d8d5bd92794225583fd4
Mateusz Markowski <mmarkowski955@gmail.com>
2021-11-10 19:21:30 +0100

    Zadanie 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Calculator calculator = new Calculator();
+        calculator.add(6,19.5);
+        calculator.sub(27.55,12.30);
 
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public void add(double a, double b){
+        System.out.println(a+b);
+    }
+    public void sub(double a, double b){
+        System.out.println(a-b);
+    }
+}
