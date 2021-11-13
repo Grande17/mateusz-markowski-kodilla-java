2e1a1798d117c1a18352609b5b3d2f99088d8fa1
Mateusz Markowski <mmarkowski955@gmail.com>
2021-11-13 19:57:36 +0100

    6.5 Mockowanie obiektow

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -0,0 +1,48 @@
+package com.kodilla.testing.weather.stub;
+
+import java.util.*;
+
+public class WeatherForecast {
+    private Temperatures temperatures;
+
+    public WeatherForecast(Temperatures temperatures) {
+        this.temperatures = temperatures;
+    }
+
+    public Map<String, Double> calculateForecast() {
+        Map<String, Double> resultMap = new HashMap<>();
+
+        for (Map.Entry<String, Double> temperature : temperatures.getTemparatures().entrySet()) {
+            resultMap.put(temperature.getKey(), temperature.getValue() + 1.0);
+        }
+        return resultMap;
+    }
+
+    public double getAverageTemperature() {
+        double sum = 0;
+        for (Map.Entry<String, Double> i : temperatures.getTemparatures().entrySet()) {
+            i.getValue();
+            sum += i.getValue();
+        }
+        double avg = sum / temperatures.getTemparatures().size();
+        return avg;
+
+    }
+
+    public double getMediana() {
+        double mediana = 0;
+        List<Double> onlyTemp = new ArrayList<>();
+
+        for (Map.Entry<String, Double> temp : temperatures.getTemparatures().entrySet()) {
+            temp.getValue();
+            onlyTemp.add(temp.getValue());
+        }
+        Collections.sort(onlyTemp);
+        if (onlyTemp.size() % 2 == 0) {
+            mediana = onlyTemp.get(onlyTemp.size() / 2 - 1);
+        } else {
+            mediana = onlyTemp.get((onlyTemp.size() / 2));
+        }
+        return mediana;
+    }
+}
