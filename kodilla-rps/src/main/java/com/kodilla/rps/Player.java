2e011002f5e1b4ebfb2be60b3e234171dd34f890
Mateusz Markowski <mmarkowski955@gmail.com>
2021-11-20 15:47:34 +0100

    7.7 RPS still in progress

---
+++ kodilla-rps/src/main/java/com/kodilla/rps/Player.java
@@ -0,0 +1,48 @@
+package com.kodilla.rps;
+
+import java.util.Scanner;
+
+public class Player {
+    private Scanner sc = new Scanner(System.in);
+    private String playerName;
+    private int numberOfWonGamesToEnd;
+    private int playerChoice;
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        System.out.println("Please set player name: ");
+        this.playerName = sc.nextLine();
+    }
+
+    public int getNumberOfWonGamesToEnd() {
+        return numberOfWonGamesToEnd;
+    }
+
+    public void setNumberOfWonGamesToEnd(int numberOfWonGamesToEnd) {
+        System.out.println("Set number of games you want to play to win: ");
+        this.numberOfWonGamesToEnd = sc.nextInt();
+    }
+
+    public int getPlayerChoice() {
+        return playerChoice;
+    }
+
+    public void setPlayerChoice(int playerChoice) {
+        this.playerChoice = sc.nextInt();
+    }
+    public void playerChoiceConditions(int playerChoice){
+        String player = "";
+        if(playerChoice == 1){
+            player = "Rock";
+        }else if(playerChoice == 2){
+            player = "Paper";
+        }else if(playerChoice == 3){
+            player = "Scissors";
+        }
+        System.out.println("Your choice was "+ player);
+    }
+
+}
