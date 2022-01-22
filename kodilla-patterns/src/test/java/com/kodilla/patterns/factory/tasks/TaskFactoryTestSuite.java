1273aaefd1e406de5d0c4be5d34548dd4bcdff27
Grande17 <mmarkowski955@gmail.com>
2022-01-22 15:58:56 +0100

    15.4

--- kodilla-patterns/src/test/java/com/kodilla/patterns/factory/tasks/TaskFactoryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/factory/tasks/TaskFactoryTestSuite.java
@@ -12,6 +12,7 @@ public class TaskFactoryTestSuite {
         TaskFactory factory = new TaskFactory();
         //When
         Task shopping = factory.makeTask(TaskFactory.SHOPPING);
+        shopping.executeTask();
         //Then
         assertEquals("Grocery", shopping.getTaskName());
         assertTrue(shopping.isTaskExecuted());
@@ -22,6 +23,7 @@ public class TaskFactoryTestSuite {
         TaskFactory factory = new TaskFactory();
         //When
         Task driving = factory.makeTask(TaskFactory.DRIVING);
+        driving.executeTask();
         //Then
         assertEquals("JohnSmith",driving.getTaskName());
         assertFalse(driving.isTaskExecuted());
@@ -32,6 +34,7 @@ public class TaskFactoryTestSuite {
         TaskFactory factory = new TaskFactory();
         //When
         Task painting = factory.makeTask(TaskFactory.PAINTING);
+        painting.executeTask();
         //Then
         assertEquals("Help", painting.getTaskName());
         assertTrue(painting.isTaskExecuted());
