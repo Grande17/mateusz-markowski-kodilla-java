17315f91b044da35c7ede052b5c1c7a246bbd7ad
Grande17 <mmarkowski955@gmail.com>
2022-05-05 16:04:41 +0200

    x

--- kodilla-sudoku/src/main/java/com/kodilla/sudoku/board/Prototype.java
+++
@@ -1,9 +0,0 @@
-package com.kodilla.sudoku.board;
-
-public class Prototype implements Cloneable{
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-}
